f5cf1515c158225ae46820b43c0fa522ffd18a28
marek4882 <cwioro200@gmail.com>
2024-10-03 22:44:43 +0200

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import HomePage from "./pages/HomePage.tsx";
+import React from "react";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <HomePage />
-  </StrictMode>
+  <React.StrictMode>
+    <App></App>
+  </React.StrictMode>
 );
