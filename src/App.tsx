c93ea33b9d1c58d277218c11eba34e85fd2bf26c
marek4882 <cwioro200@gmail.com>
2024-10-05 18:46:07 +0200

    add two routes VehiclePage and DocsPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 import SignInPage from "./pages/SignInPage";
+import DocsPage from "./pages/DocsPage";
+import VehiclePage from "./pages/VehiclePage";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/signin" element={<SignInPage />} />
+          <Route path="/vehicle" element={<VehiclePage />} />
+          <Route path="/docs" element={<DocsPage />} />
         </Routes>
       </BrowserRouter>
     </>
