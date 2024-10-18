63ca3cf1ef85bf254a301e11bd5b5331f384dd7b
marek4882 <cwioro200@gmail.com>
2024-10-18 16:16:29 +0200

    fixing view issue

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -24,47 +24,51 @@ function HomePage() {
           </picture>
         </div>
       </section>
-      <div className="block block--dark block--skewed-right hero">
-        <header>
+      <div className="block block--dark  hero">
+        <header className="block__header">
           <h2>Features</h2>
         </header>
       </div>
       <section className="block">
-        <div className="container grid grid--1x2">
-          <header className="block__header hero__content">
-            <h1 className="block__heading">Reminder</h1>
-            <p className="hero__tagline">
-              Welcome to OCR. An app where you don't have to worry when you have
-              to change the oil in your vehicle. We'll do it for you!
-            </p>
-          </header>
+        <div className="container grid grid--1x2 feature ">
+          <div className="feature__content">
+            <header className="block__header hero__content">
+              <h2 className="block__heading">Reminder</h2>
+              <p className="hero__tagline">
+                Welcome to OCR. An app where you don't have to worry when you
+                have to change the oil in your vehicle. We'll do it for you!
+              </p>
+            </header>
+          </div>
           <picture className="hero__image-container">
             <img
-              src="src\assets\hero_images.svg"
-              className="hero__image"
+              src="src\assets\reminder-graphic.svg"
+              className="feature__image"
               alt="Hero illustration"
             />
           </picture>
         </div>
-        <div className="container grid grid--1x2">
+        <div className="container grid grid--1x2 feature ">
+          <div className="feature__content">
+            <header className="block__header hero__content">
+              <h2 className="block__heading">History Tracking</h2>
+              <p className="hero__tagline">
+                Welcome to OCR. An app where you don't have to worry when you
+                have to change the oil in your vehicle. We'll do it for you!
+              </p>
+            </header>
+          </div>
           <picture className="hero__image-container">
             <img
-              src="src\assets\hero_images.svg"
-              className="hero__image"
+              src="src\assets\history-tracking.svg"
+              className="feature__image"
               alt="Hero illustration"
             />
           </picture>
-          <header className="block__header hero__content">
-            <h1 className="block__heading">Welcome To Oil Change Reminder</h1>
-            <p className="hero__tagline">
-              Welcome to OCR. An app where you don't have to worry when you have
-              to change the oil in your vehicle. We'll do it for you!
-            </p>
-          </header>
         </div>
       </section>
       <div className="block block--dark  hero">
-        <header>
+        <header className="block__header">
           <h2 className="block__header">See More Features</h2>
           <a className="btn btn--accent ">Start Now</a>
         </header>
