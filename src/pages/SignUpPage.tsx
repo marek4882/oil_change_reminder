03c4e209602296fb8e25c7f8bf33056437bc47c1
marek4882 <cwioro200@gmail.com>
2024-10-18 16:16:49 +0200

    .

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,101 +1,69 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
 
 function SignUpPage() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add form validation and submission logic here
-    if (formData.password !== formData.confirmPassword) {
-      alert("Passwords do not match!");
-      return;
-    }
-    console.log("Form Data: ", formData);
-    // Submit the form data to a server or backend service
-  };
-
   return (
-    <section className="block grid grid--1x2">
-      <picture className="hero__image-container">
-        <img
-          className="hero__image"
-          src="src\assets\hero_images.svg"
-          alt="Sign up hero"
-        />
-      </picture>
-      <form className="form-signin" onSubmit={handleSubmit}>
-        <h2>Sign Up</h2>
-        <div className="form-group">
-          <input
-            className="form-control"
-            type="text"
-            name="name"
-            placeholder="Name"
-            maxLength={255}
-            value={formData.name}
-            onChange={handleChange}
-            required
+    <>
+      <section className="block grid grid--1x2">
+        <picture className="hero__image-container">
+          <img
+            className="hero__image"
+            src="src\assets\hero_images.svg"
+            alt="Sign up hero"
           />
-        </div>
-        <div className="form-group">
-          <input
-            className="form-control"
-            type="email"
-            name="email"
-            placeholder="E-mail"
-            maxLength={255}
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            className="form-control"
-            type="password"
-            name="password"
-            placeholder="Password"
-            maxLength={255}
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            className="form-control"
-            type="password"
-            name="confirmPassword"
-            placeholder="Confirm Password"
-            maxLength={255}
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button className="btn btn--accent btn--form" type="submit">
-          Sign Up
-        </button>
+        </picture>
+        <form className="form-signin">
+          <h2>Sign Up</h2>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              placeholder="Name"
+              maxLength={255}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="email"
+              name="email"
+              placeholder="E-mail"
+              maxLength={255}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="password"
+              name="password"
+              placeholder="Password"
+              maxLength={255}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm Password"
+              maxLength={255}
+              required
+            />
+          </div>
+          <button className="btn btn--accent btn--form" type="submit">
+            Sign Up
+          </button>
 
-        <p className="muted">Copyright &copy; 2024</p>
-        <p className="signin-message">
-          Already have an account? <Link to="/signin">Sign in</Link>
-        </p>
-      </form>
-    </section>
+          <p className="muted">Copyright &copy; 2024</p>
+          <p className="signin-message">
+            Already have an account? <Link to="/signin">Sign in</Link>
+          </p>
+        </form>
+      </section>
+    </>
   );
 }
 
