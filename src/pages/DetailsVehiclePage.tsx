674fdc48806705a6d33c9aa4b36edab40b638268
marek4882 <cwioro200@gmail.com>
2024-11-06 21:19:21 +0100

    added history section

--- src/pages/DetailsVehiclePage.tsx
+++ src/pages/DetailsVehiclePage.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { Car } from "../models/Car";
+import { Car, OilType, TypeFuel, Viscosity } from "../models/Car";
 import { CarManager } from "../services/CarService";
 import { LocalRepository } from "../api/ApiService";
-import carImage from "../assets/ford-mustang.png";
+import carImage from "../assets/hero_images.svg";
+import { format } from "date-fns";
+import { Calendar, Drop, Gauge, PlusCircle } from "phosphor-react";
 
 const DetailsVehiclePage: React.FC = () => {
   const { carId } = useParams<{ carId: string }>();
@@ -13,6 +15,13 @@ const DetailsVehiclePage: React.FC = () => {
 
   const [car, setCar] = useState<Car | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+
+  const [showOilChangeForm, setShowOilChangeForm] = useState(false);
+
+  const [oilChangeDate, setOilChangeDate] = useState("");
+  const [oilType, setOilType] = useState<OilType>("Synthetic");
+  const [mileage, setMileage] = useState<number>(0);
+  const [viscosity, setViscosity] = useState<Viscosity | null>(null);
 
   useEffect(() => {
     if (carId) {
@@ -45,6 +54,36 @@ const DetailsVehiclePage: React.FC = () => {
       carManager.deleteCar(car.id);
       navigate("/vehicle"); // Redirect to home or cars list after deletion
     }
+  };
+
+  const handleAddOilChange = () => {
+    setShowOilChangeForm(true);
+  };
+
+  const handleSaveOilChange = () => {
+    if (car && oilChangeDate && mileage) {
+      const parsedDate = new Date(oilChangeDate);
+      carManager.addOilChange(car.id, parsedDate, oilType, mileage);
+      refreshCarDetails();
+      setShowOilChangeForm(false);
+    }
+  };
+  const fuelTypes = ["Petrol", "Diesel", "Hybrid", "Electric"] as const;
+  const oilTypes: Record<TypeFuel, OilType[]> = {
+    Petrol: ["Synthetic", "Semi-synthetic", "Mineral"],
+    Diesel: ["Diesel-special", "Semi-synthetic", "Mineral"],
+    Hybrid: ["Synthetic", "Hybrid-special"],
+    Electric: ["Electric-coolant"],
+  };
+  const oilToViscosityMap: Record<OilType, Viscosity[]> = {
+    Synthetic: ["0W-20", "5W-30", "10W-40"],
+    "Semi-synthetic": ["5W-30", "10W-40", "15W-40"],
+    Mineral: ["10W-40", "15W-40", "20W-50"],
+    "Diesel-special": ["10W-40", "15W-40"],
+    "Hybrid-special": ["0W-20", "5W-30"],
+    "Electric-coolant": [],
+    Conventional: [],
+    Blend: [],
   };
 
   if (loading) {
@@ -94,6 +133,14 @@ const DetailsVehiclePage: React.FC = () => {
                   {car.currentMilleage} {car.milleageUnit}
                 </span>
               </p>
+              <p>
+                Kolejna Wymiana Oleju:
+                <span className="decoration">
+                  {car.nextOilChangeDate
+                    ? format(new Date(car.nextOilChangeDate), "yyyy-MM-dd")
+                    : "Brak danych"}
+                </span>
+              </p>
               <button className="btn btn--edit" onClick={handleEdit}>
                 Edit
               </button>
@@ -104,37 +151,92 @@ const DetailsVehiclePage: React.FC = () => {
           </section>
           <section className="block">
             <div className="timeline-container">
-              <div className="timeline-event">
-                <div className="timeline-date">2020</div>
-                <div className="timeline-content">
-                  <h3 className="event-title">Event Title 1</h3>
-                  <p className="event-description">
-                    This is a description of the first event. It took place in
-                    2020.
-                  </p>
-                </div>
-              </div>
-              <div className="timeline-event">
-                <div className="timeline-date">2021</div>
-                <div className="timeline-content">
-                  <h3 className="event-title">Event Title 2</h3>
-                  <p className="event-description">
-                    This is a description of the second event. It took place in
-                    2021.
-                  </p>
-                </div>
-              </div>
-              <div className="timeline-event">
-                <div className="timeline-date">2022</div>
-                <div className="timeline-content">
-                  <h3 className="event-title">Event Title 3</h3>
-                  <p className="event-description">
-                    This is a description of the third event. It took place in
-                    2022.
-                  </p>
-                </div>
-              </div>
+              <h3 className="block__header">History</h3>
+              <button className="btn" onClick={handleAddOilChange}>
+                Add Oil Change
+              </button>
+              {car.oilChangeHistory && car.oilChangeHistory.length > 0 ? (
+                car.oilChangeHistory.map((record, index) => (
+                  <div key={index} className="timeline-event">
+                    <div className="timeline-date">
+                      <PlusCircle size={32} />
+                    </div>
+                    <div className="timeline-content">
+                      <p className="event-description">
+                        <Calendar size={20} color="black" /> Data Wymiany Oleju:{" "}
+                        <span className="decoration">
+                          {format(new Date(record.date), "dd.MM.yyyy")}
+                        </span>
+                      </p>
+                      <p className="event-description">
+                        <Drop size={20} color="black" /> Typ Oleju:{" "}
+                        <span className="decoration">{record.oilType} </span>
+                      </p>
+                      <p className="event-description">
+                        <Gauge size={20} color="black" /> Przebieg:{" "}
+                        <span className="decoration">{record.mileage}</span>
+                      </p>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <p>No oil change history available.</p>
+              )}
             </div>
+
+            {/* Oil Change Form */}
+            {showOilChangeForm && (
+              <div className="oil-change-form">
+                {/* Last Oil Change Date Field */}
+                <div>
+                  <label>Last Oil Change Date:</label>
+                  <input
+                    className="form-control"
+                    type="date"
+                    value={oilChangeDate}
+                    onChange={(e) => setOilChangeDate(e.target.value)}
+                    required
+                  />
+                </div>
+                {/* Oil Type Dropdown */}
+                <div>
+                  <label>Oil Type:</label>
+                  <select
+                    className="form-control"
+                    value={oilType}
+                    onChange={(e) => setOilType(e.target.value as OilType)}
+                  >
+                    {car?.typeFuel &&
+                      oilTypes[car.typeFuel].map((oil) => (
+                        <option key={oil} value={oil}>
+                          {oil}
+                        </option>
+                      ))}
+                  </select>
+                </div>
+
+                {/* Current Mileage Field */}
+                <div>
+                  <label>Current Mileage:</label>
+                  <input
+                    className="form-control"
+                    type="number"
+                    value={mileage}
+                    onChange={(e) => setMileage(Number(e.target.value))}
+                    required
+                  />
+                </div>
+                <button className="btn btn--save" onClick={handleSaveOilChange}>
+                  Save
+                </button>
+                <button
+                  className="btn btn--cancel"
+                  onClick={() => setShowOilChangeForm(false)}
+                >
+                  Cancel
+                </button>
+              </div>
+            )}
           </section>
         </>
       ) : (
