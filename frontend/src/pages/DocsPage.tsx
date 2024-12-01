0fb0695bfbdee68707dcb69616b0b6e00da2426c
marek4882 <cwioro200@gmail.com>
2024-12-01 23:57:02 +0100

    written documentation on the client side

--- frontend/src/pages/DocsPage.tsx
+++ frontend/src/pages/DocsPage.tsx
@@ -66,6 +66,7 @@ function DocsPage() {
         <details className="block">
           <summary>How to edit a car?</summary>
           <h3>Edit a Car</h3>
+          <p>I/II</p>
           <ol>
             <li>
               Go to <a href="/vehicle">"My Vehicle"</a> section.
@@ -76,7 +77,23 @@ function DocsPage() {
           </ol>
           <video
             className="block__video"
-            src="\src\assets\video\sign_up_video.mp4"
+            src="\src\assets\video\how_to_edit_car_I_II.mp4"
+            autoPlay
+            controls
+          ></video>
+          <p>II/II</p>
+          <ol>
+            <li>
+              Go to <a href="/vehicle">"My Vehicle"</a> section.
+            </li>
+            <li>Click the details button</li>
+            <li>Click the edit button</li>
+            <li>Enter new information</li>
+            <li>After entering data, click edit button.</li>
+          </ol>
+          <video
+            className="block__video"
+            src="\src\assets\video\how_to_edit_car_II_II.mp4"
             autoPlay
             controls
           ></video>
@@ -84,20 +101,34 @@ function DocsPage() {
         <details className="block">
           <summary>How to delete a car?</summary>
           <h3>Delete a car</h3>
+          <p>I/II</p>
           <ol>
-            <li>Click the sign in button</li>
-            <li>Click the sigm up button at the bottom of the form</li>
-            <li>Enter your information</li>
-            <li>After entering your data, click sign up.</li>
-            <li>After entering your data, click sign up.</li>
             <li>
-              After successful registration, you can add your vehicles in the
-              <a href="/vehicle">"My vehicle" </a>section
+              Go to <a href="/vehicle">"My Vehicle"</a> section.
             </li>
+            <li>Click the delete button</li>
+            <li>After successful deletion, you should not see your car.</li>
           </ol>
+
           <video
             className="block__video"
-            src="\src\assets\video\sign_up_video.mp4"
+            src="\src\assets\video\how_to_delete_car_I_II.mp4"
+            autoPlay
+            controls
+          ></video>
+          <p>II/II</p>
+          <ol>
+            <li>
+              Go to <a href="/vehicle">"My Vehicle"</a> section.
+            </li>
+            <li>Click the details button</li>
+            <li>Click the delete button</li>
+            <li>After successful deletion, you should not see your car.</li>
+          </ol>
+
+          <video
+            className="block__video"
+            src="\src\assets\video\how_to_delete_car_II_II.mp4"
             autoPlay
             controls
           ></video>
@@ -106,63 +137,57 @@ function DocsPage() {
           <summary>
             Where to find information about the next oil change?
           </summary>
-          <h3>Sign Up</h3>
+          <h3>Finding information about the next oil change</h3>
           <ol>
-            <li>Click the sign in button</li>
-            <li>Click the sigm up button at the bottom of the form</li>
-            <li>Enter your information</li>
-            <li>After entering your data, click sign up.</li>
-            <li>After entering your data, click sign up.</li>
             <li>
-              After successful registration, you can add your vehicles in the
-              <a href="/vehicle">"My vehicle" </a>section
+              Go to <a href="/vehicle">"My Vehicle"</a> section.
+            </li>
+            <li>There is a field called Next Oil Change</li>
+            <li>
+              Also you can find this information after click details button
             </li>
           </ol>
           <video
             className="block__video"
-            src="\src\assets\video\sign_up_video.mp4"
+            src="\src\assets\video\where_i_can_find_the_next_oil_change_information.mp4"
             autoPlay
             controls
           ></video>
         </details>
         <details className="block">
           <summary>How to add next oil change?</summary>
-          <h3>Sign Up</h3>
+          <h3>Adding next oil change</h3>
           <ol>
-            <li>Click the sign in button</li>
-            <li>Click the sigm up button at the bottom of the form</li>
-            <li>Enter your information</li>
-            <li>After entering your data, click sign up.</li>
-            <li>After entering your data, click sign up.</li>
             <li>
-              After successful registration, you can add your vehicles in the
-              <a href="/vehicle">"My vehicle" </a>section
+              Go to <a href="/vehicle">"My Vehicle"</a> section.
             </li>
+            <li>Click the details button</li>
+            <li>Go to the bottom of the page called History</li>
+            <li>Click Add Oil Change button</li>
+            <li>Enter information</li>
+            <li>Now you can see new date your next oil change</li>
           </ol>
           <video
             className="block__video"
-            src="\src\assets\video\sign_up_video.mp4"
+            src="\src\assets\video\how_to_add_next_oil_change.mp4"
             autoPlay
             controls
           ></video>
         </details>
         <details className="block">
           <summary>Where can I find the oil change history?</summary>
-          <h3>Sign Up</h3>
+          <h3>Finding information about oil change history</h3>
           <ol>
-            <li>Click the sign in button</li>
-            <li>Click the sigm up button at the bottom of the form</li>
-            <li>Enter your information</li>
-            <li>After entering your data, click sign up.</li>
-            <li>After entering your data, click sign up.</li>
             <li>
-              After successful registration, you can add your vehicles in the
-              <a href="/vehicle">"My vehicle" </a>section
+              Go to <a href="/vehicle">"My Vehicle"</a> section.
             </li>
+            <li>Click the details button</li>
+            <li>Go to the bottom of the page called History</li>
+            <li>Now you can see the information about oil change</li>
           </ol>
           <video
             className="block__video"
-            src="\src\assets\video\sign_up_video.mp4"
+            src="\src\assets\video\where_i_can_find_history_of_the_oil_change.mp4"
             autoPlay
             controls
           ></video>
